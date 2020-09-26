eca3e38e81fa75bfacdd2ea7b811ae5860b1c6b2
Munir Mahmud <munirmahmud6@gmail.com>
2020-09-26 09:46:50 +0600

    Facebook story updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "./components/Header/Header";
 import Sidebar from './components/Sidebar/Sidebar';
+import Feed from './components/Feed/Feed';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 
         <div className="app-body d-flex">
             <Sidebar />
+            <Feed />
             {/* Widgets */}
         </div>
     </div>
